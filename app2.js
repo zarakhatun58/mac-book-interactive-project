b77e4b8bd3d63a69ea8fd9cb68858c04ef131178
jahanara khatun <zarakhatun58@gmail.com>
2021-08-21 22:09:37 +0530

    all html pomo code id add

--- app2.js
+++ app2.js
@@ -25,38 +25,38 @@ function discountedTotal() {
     if (matchCode.value == 'stevekaku') {
         const TotalPriceText = document.getElementById('total-price');
         const TotalPrice = parseFloat(TotalPriceText.innerText)
-        const discount = TotalPrice / 5;
+        const discount = TotalPrice / 20;
         const discountedTotal = TotalPrice - discount;
         document.getElementById('final-total').innerText = discountedTotal;
         matchCode.value = '';
     }
 }
 // getting 8gb memory button event-handler
-document.getElementById('8gb-btn').addEventListener('click', function () {
+document.getElementById('button-8gb').addEventListener('click', function () {
     updatePrice('extra-memory', 0)
 })
 // getting 16gb memory button event-handler
-document.getElementById('16gb-btn').addEventListener('click', function () {
+document.getElementById('button-16gb').addEventListener('click', function () {
     updatePrice('extra-memory', 180)
 })
 // getting 256gb storage button event-handler
-document.getElementById('256gb-btn').addEventListener('click', function () {
+document.getElementById('button-256gb').addEventListener('click', function () {
     updatePrice('extra-storage', 0)
 })
 // getting 512gb storage button event-handler
-document.getElementById('512gb-btn').addEventListener('click', function () {
+document.getElementById('button-512gb').addEventListener('click', function () {
     updatePrice('extra-storage', 100)
 })
 // getting 1TB storage button event-handler
-document.getElementById('1tb-btn').addEventListener('click', function () {
+document.getElementById('button-1tb').addEventListener('click', function () {
     updatePrice('extra-storage', 180)
 })
 // getting aug21 button event-handler
-document.getElementById('aug25-btn').addEventListener('click', function () {
+document.getElementById('button-aug25').addEventListener('click', function () {
     updatePrice('delivery-cost', 0)
 })
 // getting aug21 button event-handler
-document.getElementById('aug21-btn').addEventListener('click', function () {
+document.getElementById('button-aug21').addEventListener('click', function () {
     updatePrice('delivery-cost', 20)
 })
 // apply button event-handler
